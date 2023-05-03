33380ba8ecde65316cb4e8caf60e14b29c46918d
Roughyy <mrafiihaditomo123@gmail.com>
2023-05-03 16:40:35 +0700

    Update Database Models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+admin.site.register(PersonalPrediction)
+admin.site.register(UserHistory)
+admin.site.register(CoinCategory)
+admin.site.register(LSTMModel)
+admin.site.register(ProphetScore)
+admin.site.register(Cryptocurrencies)
+admin.site.register(SystemPrediction)
